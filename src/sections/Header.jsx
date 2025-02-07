ca9bd7ed9405c58bb17c00161b407555797594d0
calebmutisya <mutisyacalebmusau@gmail.com>
2025-02-07 12:06:56 +0300

    added scrolling effect

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import '../main.css'
 import Button from '../components/Button'
 import logo from '../assets/icons/logo.svg'
 
 function Header() {
+
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <nav>
+      <nav className={scrolled ? 'scrolled' : ''}>
         <img className='logo' src={logo} alt="webcrafters" />
         <ul className='sections'>
           <li>Services</li>
           <li>Portfolio</li>
           <li>Contact</li>
         </ul>
-        < Button text="Let's Talk" />
+        <Button text="Let's Talk" />
       </nav>
     </>
   )
